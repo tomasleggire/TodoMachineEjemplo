70509d3113af8bc8a5c7af329cafffef1777eda1
Tomás Leggire <tomasleggire@hotmail.com>
2022-09-25 20:51:07 -0300

    Ultima version del machine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import './App.css';
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+
+const todos = [
+  {text: "Cortar cebolla", completed: false},
+  {text: "Completar el curso de react", completed: false},
+  {text: "Llorar todo el dia", completed: false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>      
+        {todos.map(todo => (
+          <TodoItem text={todo.text} key={todo.text}/>
+        ))}     
+      </TodoList>
+
+      <CreateTodoButton />
+
+    </React.Fragment>
   );
 }
 
