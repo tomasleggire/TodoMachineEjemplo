c38c4c04aa12f14fe30e678faff1a5358d7b1a72
Tomás Leggire <tomasleggire@hotmail.com>
2022-09-30 21:59:29 -0300

    Ordenado y funciones terminadas de localStorage

---
+++ src/App/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+import { AppUI } from "./AppUI";
+
+// const defaultTodos = [
+//   {text: "Cortar cebolla", completed: true},
+//   {text: "Completar el curso de react", completed: false},
+//   {text: "Llorar todo el dia", completed: false},
+//   {text: "Llorar todo el dia de nuevo", completed: false}
+// ]
+
+function App() {
+   
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+  let parsedTodos;
+
+  if(!localStorageTodos) {
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+  
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo =>{
+      const todoText = todo.text.toLocaleLowerCase();
+      const searchText = searchValue.toLocaleLowerCase();
+      return todoText.includes(searchText);
+    })
+  }
+
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem("TODOS_V1", stringifiedTodos);
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    if (newTodos[todoIndex].completed == true) {
+      newTodos[todoIndex].completed = false
+    } else {
+      newTodos[todoIndex].completed = true
+    };
+    saveTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  }
+
+  return (
+    <AppUI
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -3,19 +3,11 @@ import "./TodoItem.css";
 
 function TodoItem(props) {
 
-const onComplete = () => {
-    alert("Completaste el todo " + props.text);
-};
-
-const onDelete = () => {
-    alert("Borraste el todo " + props.text);
-};
-
 return (
     <li className="TodoItem">
       <span 
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={onComplete}
+        onClick={props.onComplete}
       >
         √
 
@@ -23,7 +15,7 @@ return (
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
       <span
         className="Icon Icon-delete"
-        onClick={onDelete}
+        onClick={props.onDelete}
       >
         X
 
